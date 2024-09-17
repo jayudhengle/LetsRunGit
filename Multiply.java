6c0fdb55b5b0fe84f2fd3ab88a75cafa2952f537
Jayu Dhengle <Jayu.Dhengle@gmail.com>
2024-09-17 18:38:01 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply
+{
+    public static void main(String [] args)
+    {
+        int a= 6;
+        int b =7;
+
+        System.out.println(" the product is " + (a*b));
+    }
+}
